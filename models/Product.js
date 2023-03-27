4d3dd39e9628511358d93c931cc333430d2ea202
MasonLagrange <mason.lagrange@gmail.com>
2023-03-26 22:50:07 -0400

    added models and .env

--- models/Product.js
+++ models/Product.js
@@ -2,6 +2,8 @@
 const { Model, DataTypes } = require('sequelize');
 // import our database connection from config.js
 const sequelize = require('../config/connection');
+const Category = require('./Category');
+const Tag = require('./Tag');
 
 // Initialize Product model (table) by extending off Sequelize's Model class
 class Product extends Model {}
@@ -9,7 +11,34 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INT,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        isDecimal: true
+      }
+    },
+    stock: {
+      type: DataTypes.INT,
+      allowNull: false,
+      default: 10,
+      validate: {
+        isNumeric: true
+      }
+    },
+    category_id: {
+      type: DataTypes.INT,
+    }.belongsTo(Category, { foreignKey: 'id'})
   },
   {
     sequelize,
@@ -19,5 +48,6 @@ Product.init(
     modelName: 'product',
   }
 );
-
+Product.belongsTo(Category)
+Product.HasMany(Tag)
 module.exports = Product;
